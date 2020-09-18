ba52a124fd962c1c082a9e9bd60cf6c628a87a22
Chi-Chang Lee <r08922a27@csie.ntu.edu.tw>
2020-09-19 00:39:31 +0800

    update model.py/config.yml

--- model.py
+++ model.py
@@ -4,18 +4,20 @@ from functools import partial
 
 MAX_POSITIONS_LEN = 16000 * 50
 
+
 class SpecBase(nn.Module):
     def __init__(self, loss_func, preprocessor, input_size, hidden_size, num_layers, bidirectional):
         super(SpecBase, self).__init__()
-        self.bidirectional = bidirectional
+        self.bidirectional, self.hidden_size = bidirectional, hidden_size
         self.lstm = nn.LSTM(input_size=input_size, hidden_size=hidden_size,
                             num_layers=num_layers, batch_first=True, bidirectional=self.bidirectional)
         self.loss_func = loss_func
         self.preprocessor = preprocessor
 
-        self._clamp_args = {'max': self.preprocessor._win_args['win_length'], 'min': 0}
+        self._clamp_args = {
+            'max': self.preprocessor._win_args['win_length'], 'min': 0}
         self.clamp = partial(torch.clamp, **self._clamp_args)
-        
+
         self.scaling_layer = nn.Sequential(
             nn.Linear(hidden_size, input_size))
         self.init_weights()
@@ -32,71 +34,81 @@ class SpecBase(nn.Module):
     def infer(self, src):
         pred_linears, src_phases = self.transform(src)
         return self.preprocessor.istft(linears=pred_linears, phases=src_phases, length=src.shape[-1])
-    
+
     def _get_length_masks(self, lengths):
         # lengths: (batch_size, ) in cuda
         ascending = torch.arange(MAX_POSITIONS_LEN).to(device=lengths.device)
-        ascending = ascending[:lengths.max().item()].unsqueeze(0).expand(len(lengths), -1)
+        ascending = ascending[:lengths.max().item()].unsqueeze(
+            0).expand(len(lengths), -1)
         length_masks = (ascending < lengths.unsqueeze(-1)).long()
         return length_masks.unsqueeze(-1)
 
-    def forward(self, lengths, src, tar):
-        pred_linears, tar_linears = self.transform(src, tar)
-        
-        stft_lengths = lengths // self.preprocessor._win_args['hop_length'] + 1
-        stft_length_masks = self._get_length_masks(stft_lengths)
-                    
-        pred_linears, tar_linears = pred_linears * stft_length_masks, tar_linears * stft_length_masks
+    def _lstm(self, src_linears):
+        pred_linears, _ = self.lstm(src_linears)
+        if self.bidirectional:
+            pred_linears = pred_linears[:, :, :self.hidden_size] + \
+                pred_linears[:, :, self.hidden_size:]
+        return pred_linears
+
+    def compute_loss(self, pred_linears, tar_linears, stft_length_masks):
+        pred_linears, tar_linears = pred_linears * \
+            stft_length_masks, tar_linears * stft_length_masks
         return self.loss_func(pred_linears.flatten(start_dim=1).contiguous(),
                               tar_linears.flatten(start_dim=1).contiguous())
 
-class LSTM(SpecBase):
-    def __init__(self, loss_func, preprocessor, input_size, hidden_size, num_layers, bidirectional):
-        super(LSTM, self).__init__(loss_func, preprocessor, input_size, hidden_size, num_layers, bidirectional)
-        
     def transform(self, src, tar=None):
         _, src_linears, src_phases = self.preprocessor(src)
-        pred_linears, _ = self.lstm(src_linears)
-        pred_linears = self.scaling_layer(pred_linears)
-        pred_linears = self.clamp(pred_linears)
+        pred_linears = self.denoise(src_linears)
 
         if tar is not None:
             _, tar_linears, _ = self.preprocessor(tar)
             return pred_linears, tar_linears
         else:
             return pred_linears, src_phases
+
+    def forward(self, lengths, src, tar):
+        stft_lengths = lengths // self.preprocessor._win_args['hop_length'] + 1
+        stft_length_masks = self._get_length_masks(stft_lengths)
+
+        pred_linears, tar_linears = self.transform(src, tar)
+
+        return self.compute_loss(pred_linears, tar_linears, stft_length_masks)
+
+
+class LSTM(SpecBase):
+    def __init__(self, loss_func, preprocessor, input_size, hidden_size, num_layers, bidirectional):
+        super(LSTM, self).__init__(loss_func, preprocessor,
+                                   input_size, hidden_size, num_layers, bidirectional)
+
+    def denoise(self, src_linears):
+        pred_linears = self._lstm(src_linears)
+        pred_linears = self.scaling_layer(pred_linears)
+        pred_linears = self.clamp(pred_linears)
+        return pred_linears
+
 
 class IRM(SpecBase):
     def __init__(self, loss_func, preprocessor, input_size, hidden_size, num_layers, bidirectional):
-        super(IRM, self).__init__(loss_func, preprocessor, input_size, hidden_size, num_layers, bidirectional)
+        super(IRM, self).__init__(loss_func, preprocessor,
+                                  input_size, hidden_size, num_layers, bidirectional)
         self.scaling_layer = nn.Sequential(
             nn.Linear(hidden_size, input_size), nn.Sigmoid())
-  
-    def transform(self, src, tar=None):
-        _, src_linears, src_phases = self.preprocessor(src)
-        pred_masks, _ = self.lstm(src_linears)
+
+    def denoise(self, src_linears):
+        pred_masks = self._lstm(src_linears)
         pred_masks = self.scaling_layer(pred_masks)
         pred_linears = src_linears * pred_masks
+        return pred_linears
 
-        if tar is not None:
-            _, tar_linears, _ = self.preprocessor(tar)
-            return pred_linears, tar_linears
-        else:
-            return pred_linears, src_phases
 
 class Residual(SpecBase):
     def __init__(self, loss_func, preprocessor, input_size, hidden_size, num_layers, bidirectional):
-        super(Residual, self).__init__(loss_func, preprocessor, input_size, hidden_size, num_layers, bidirectional)
-        
-    def transform(self, src, tar=None):
-        _, src_linears, src_phases = self.preprocessor(src)
-        pred_masks, _ = self.lstm(src_linears)
+        super(Residual, self).__init__(loss_func, preprocessor,
+                                       input_size, hidden_size, num_layers, bidirectional)
+
+    def denoise(self, src_linears):
+        pred_masks = self._lstm(src_linears)
         pred_masks = self.scaling_layer(pred_masks)
         pred_linears = src_linears * pred_masks
         pred_linears = self.clamp(pred_linears)
-
-        if tar is not None:
-            _, tar_linears, _ = self.preprocessor(tar)
-            return pred_linears, tar_linears
-        else:
-            return pred_linears, src_phases
+        return pred_linears
